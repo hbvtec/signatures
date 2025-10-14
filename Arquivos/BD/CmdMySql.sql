7c1e814f26ea451385b199ea1b405162d0080615
HBVTEC <hbvtec74@gmail.com>
2025-10-14 16:14:21 -0300

    20251014
    Remoção arquivos PC trabalho.

---
+++ Arquivos/BD/CmdMySql.sql
@@ -0,0 +1,34 @@
+﻿use mysql;
+
+GRANT ALL ON *.* TO root@'192.168.101.168' IDENTIFIED BY 'root';
+GRANT ALL ON *.* TO root@'192.168.101.97' IDENTIFIED BY 'root';
+
+use ABBR;
+
+create table tblClientes (
+	idCliente	int	not null primary key,
+	nmCliente	varchar(40)
+);
+
+INSERT INTO tblClientes
+values
+(2, 'Helton'),
+(3, 'Luiz');
+
+DROP PROCEDURE IF EXISTS `spClientes_Sel`;
+
+CREATE PROCEDURE `spClientes_Sel`(IN prmIdCliente INT)
+BEGIN
+	IF(prmIdCliente IS NULL) THEN
+		SELECT	idCliente	as	'Código',
+				nmCliente	as	'Nome'
+		FROM	tblClientes;
+		ELSE
+			SELECT	idCliente	as	'Código',
+					nmCliente	as	'Nome'
+			FROM	tblClientes
+			where	idCliente = prmIdCliente;
+	END IF;
+END;
+
+call spClientes_Sel(null);
---
+++ Arquivos/BD/SelectReplaceCast.sql
@@ -0,0 +1,19 @@
+﻿select  cast(cod_barra as varchar(15))
+        , des_produto
+        , length(des_resumida) as Tamanho
+        , des_resumida
+        , replace(cast(vlr_venda1 as varchar(15)), '.', ',')
+        , *
+from    produto
+where   flg_ativo = 'S'
+and     length(des_resumida) > 27
+order by tamanho, cod_barra, des_produto;
+
+select   cod_empresa
+         , cod_produto
+         , cod_barra
+         , des_resumida
+         , des_produto
+from     produto
+where    flg_ativo = 'S'
+and      length(des_produto) > 27;
